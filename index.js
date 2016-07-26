c14f781a5e5eafcce4923f00c4585786d45945ff
Nathan Curtis <nathan.curtis@gmail.com>
2016-07-26 15:07:19 -0700

    Various bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Note, this makes use of some assumptions about the formatting of the HAML:
 // 1) Any opening parens or curly brace will be immediately preceded by [a-zA-Z]
-// 2) Any closing parens or curly brace that ends an attribute list is the last
+// 2) Any closing parens or curly brace that ends a multi-line attribute list is the last
 //    character on the line.
-var createPreprocessor = function(args, logger, helper) {
-  var OPEN_REGEX = /[a-zA-Z]+([\({]).*[^\)}]$/
+var createPreprocessor = function(logger) {
+  var OPEN_REGEX = /^\s*[\.%][\-_\.#a-zA-Z]+([\({])('[^']*'|"[^"]*"|[^'"\)}])*$/;
 
   var log = logger.create('preprocessor.haml-attribute-concatenation');
 
@@ -11,19 +11,22 @@ var createPreprocessor = function(args, logger, helper) {
     log.debug('Concatenating attributes in "%s".', file.originalPath);
 
     var lines = content.split(/\n/);
+    var matched = false;
     for (var ii = 0; ii < lines.length; ii++) {
       var match = lines[ii].match(OPEN_REGEX);
       if (match) {
-        var closeCharacter = match.pop() == '(' ? ')' : '}';
+        matched = true;
+        var closeCharacter = match[1] == '(' ? ')' : '}';
         var closeIndex = findClose(lines, ii, closeCharacter);
         if (closeIndex < ii) {
           log.error('No close character found [%s]!', closeCharacter);
+          continue;
         }
 
-        var attributeLines = lines.splice(ii, closeCharacter - ii);
+        var attributeLines = lines.splice(ii, closeIndex - ii + 1);
         var concatenated = attributeLines.map(function(line) { return line.trim(); }).join(' ');
         lines.splice(ii, 0, concatenated);
-        log.debug('Concatenated: %s', concatenated);
+        log.debug('Concatenated: [%s, %s] %s', ii + 1, closeIndex + 1, concatenated);
       }
     }
 
@@ -36,9 +39,9 @@ var findClose = function(lines, openIndex, closeCharacter) {
     if (lines[ii].endsWith(closeCharacter)) return ii;
   }
   return -1;
-}
+};
 
-createHamlPreprocessor.$inject = ['args', 'logger', 'helper'];
+createPreprocessor.$inject = ['logger'];
 
 // PUBLISH DI MODULE
 module.exports = {
