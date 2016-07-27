a932aa653cb14beb30fd9c959123dfe19a9c7d5b
Nathan Curtis <nathan.curtis@gmail.com>
2016-07-26 21:43:11 -0700

    Robustify a bit

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// Note, this makes use of some assumptions about the formatting of the HAML:
-// 1) Any opening parens or curly brace will be immediately preceded by [a-zA-Z]
-// 2) Any closing parens or curly brace that ends a multi-line attribute list is the last
-//    character on the line.
 var createPreprocessor = function(logger) {
   var OPEN_REGEX = /^\s*[\.%][\-_\.#a-zA-Z]+([\({])('[^']*'|"[^"]*"|[^'"\)}])*$/;
+  var CLOSE_REGEX = /^('[^']*'|"[^"]*"|[^'"])*([\)}]).*$/;
 
   var log = logger.create('preprocessor.haml-attribute-concatenation');
 
-  return function(content, file, done) {
+  var concatenateAttributeBlocks = function(content, file, done) {
     log.debug('Concatenating attributes in "%s".', file.originalPath);
 
     var lines = content.split(/\n/);
+    var matched = false;
     for (var ii = 0; ii < lines.length; ii++) {
       var match = lines[ii].match(OPEN_REGEX);
       if (match) {
+        matched = true;
         var closeCharacter = match[1] == '(' ? ')' : '}';
         var closeIndex = findClose(lines, ii, closeCharacter);
         if (closeIndex < ii) {
@@ -30,18 +29,27 @@ var createPreprocessor = function(logger) {
 
     done(lines.join('\n'));
   };
-};
 
-var findClose = function(lines, openIndex, closeCharacter) {
-  for (var ii = openIndex + 1; ii < lines.length; ii++) {
-    if (lines[ii].endsWith(closeCharacter)) return ii;
-  }
-  return -1;
+  var findClose = function(lines, openIndex, closeCharacter) {
+    for (var ii = openIndex + 1; ii < lines.length; ii++) {
+      var match = lines[ii].match(CLOSE_REGEX);
+      if (match) {
+        if (match[2] != closeCharacter) {
+          log.error('Found the wrong close character [%s]!', match);
+          return -1;
+        }
+
+        return ii;
+      }
+    }
+    return -1;
+  };
+
+  return concatenateAttributeBlocks;
 };
 
 createPreprocessor.$inject = ['logger'];
 
-// PUBLISH DI MODULE
 module.exports = {
   'preprocessor:haml-attribute-concatenation': ['factory', createPreprocessor]
 };
